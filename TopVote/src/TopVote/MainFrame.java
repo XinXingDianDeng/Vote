e4a9345bf396c2384699044cb95af3dc62080d06
c3407nb <819008851@qq.com>
2018-12-11 11:55:48 +0800

    0.1

---
+++ TopVote/src/TopVote/MainFrame.java
@@ -0,0 +1,12 @@
+package TopVote;
+
+import java.awt.*;
+import java.awt.event.*;
+public class MainFrame extends Frame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame mf = new MainFrame();
+	}
+
+}
